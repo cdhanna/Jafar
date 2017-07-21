33a19b0ae136eed3180d799e473db33232c50506
Christopher Hanna <cdhannaphone@gmail.com>
2017-07-20 21:59:07 -0400

    holy God

---
+++ Assets/JsonTest.cs
@@ -0,0 +1,26 @@
+﻿//using Newtonsoft.Json;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    public class JsonTest : MonoBehaviour
+    {
+        public void Start()
+        {
+            var json = File.ReadAllText("Assets/poop.json");
+            var data = JsonConvert.DeserializeObject<string[]>(json);
+            //var data = new string[] { "asdfasdf" };
+            foreach(var elem in data)
+            {
+                Debug.Log("DATA FROM FILE " + elem);
+            }
+        }
+    }
+}
